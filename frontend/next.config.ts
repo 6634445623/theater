6ad3124632f9c8259b62169299aeb1dc6bb30094
6634445623 <6634445623@student.chula.ac.th>
2025-05-08 18:44:33 +0000

    Add variety.com and others

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'variety.com',
+        pathname: '/wp-content/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.themoviedb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'movieposters.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudfront.net',
+        pathname: '/**',
+      }
+    ],
+  }
 };
 
 export default nextConfig;
